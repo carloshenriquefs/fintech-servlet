30dfa21e218f5e6bf30a40e619b7dbffc2b3f182
Carlos Henrique <ichfs9108@gmail.com>
2024-11-12 20:48:50 -0300

    refactor: ajustando exceptions

--- fintech/src/main/java/br/com/fiap/fintech/exceptions/ErrorTypeEnum.java
+++ fintech/src/main/java/br/com/fiap/fintech/exceptions/ErrorTypeEnum.java
@@ -1,9 +1,13 @@
 package br.com.fiap.fintech.exceptions;
 
 public enum ErrorTypeEnum {
-    ERROR_CONNECTING_TO_DATABASE,
+
     ERROR_INSERTING_DATA,
     ERROR_SEARCHING_DATA,
+    ERROR_UPDATING_DATA,
+    ERROR_DELETING_DATA,
+    ERROR_LISTING_DATA,
 
+    ERROR_CONNECTING_TO_DATABASE,
     ERROR_FETCHING_DATA;
 }
--- fintech/src/main/java/br/com/fiap/fintech/exceptions/FintechException.java
+++ fintech/src/main/java/br/com/fiap/fintech/exceptions/FintechException.java
@@ -1,12 +1,16 @@
 package br.com.fiap.fintech.exceptions;
 
-public class AppFintechException extends RuntimeException {
+public class FintechException extends RuntimeException {
     private static final long serialVersionUID = 1L;
     private ErrorTypeEnum errorType;
 
-    public AppFintechException(String msg, Throwable throwable, ErrorTypeEnum errorType) {
+    public FintechException(String msg, Throwable throwable, ErrorTypeEnum errorType) {
         super(msg, throwable);
         this.errorType = errorType;
+    }
+
+    public FintechException(String message) {
+        super(message);
     }
 
     public ErrorTypeEnum getErrorType() {
